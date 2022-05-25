6af59aa4940e3e6c0090ad682f7b2dd32df477b4
Reshma2310 <dreshma253@gmail.com>
2022-05-25 22:46:52 +0500

    [Reshma] - Creating JSON file having Inventory Data

---
+++ ObjectOrientedPrograms/ObjectOrientedPrograms/InventoryDataMain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ObjectOrientedPrograms
+{
+    internal class InventoryDataMain
+    {
+        InventoryDataModel model = new InventoryDataModel();
+        List<InventoryDataModel> inventoryDataList = new List<InventoryDataModel> ();
+
+        public void Convert(string filePath)
+        {
+            using (StreamReader file = new StreamReader(filePath))
+            {
+                var json = file.ReadToEnd();
+                var items = JsonConvert.DeserializeObject<List<InventoryDataModel>>(json);
+
+                foreach (var details in items)
+                {
+                    Console.WriteLine("Name: " + details.Name + " Price: " + details.Price + " Weight: " + details.Weight);
+                }
+            }
+        }
+    }
+}
---
+++ ObjectOrientedPrograms/ObjectOrientedPrograms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ObjectOrientedPrograms
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Json First Program");
+            InventoryDataMain json = new InventoryDataMain();
+            json.Convert(@"D:\BridgeLabs\ObjectOrientedPrograms\ObjectOrientedPrograms\ObjectOrientedPrograms\InventoryData.json");
+        }
+    }
+}
